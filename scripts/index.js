a3586037006069a4c1253426a4d044b3508989ae
Raju Malla <irwifi@gmail.com>
2017-06-29 04:02:24 +0545

    Dashboard

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,36 @@
 $( function () {
+  // To clear the localStorage Cache
+  // localStorage.clear();
+
+  var signin_info = JSON.parse(localStorage.getItem("signin"));
+  // Check if user has logged in
+  if(signin_info === null || signin_info.signing_status !== true) {
+    $("#login").show();
+  } else {
+    $("#logged_window").show();
+  }
+
   $( "#login .btn_login" ).on( "click", login_click );
 
   $( "#login input" ).on( "change", function () { $( this ).css( "border-color", "#DDD" ); } );
 
   $( "#login input" ).on( "keydown", check_enter );
-} );
+
+  $(".fa-sign-out").on("click", logout);
+
+  $(".website").on('click', function() { chrome.tabs.create({url: "https://www.timedoctor.com/"}); });
+});
 
 function login_click() {
-  if ( $( "#login .input_username" ).val() === "admin" && $( "#login .input_password" ).val() === "admin" ) {
+  if ( $( "#login .input_username" ).val().trim().toLowerCase() === "admin" && $( "#login .input_password" ).val() === "admin" ) {
+    var signin_info = {"signing_status": true, "username": $( "#login .input_username" ).val()};
+    // Save user logging in in cookie
+    localStorage.setItem("signin", JSON.stringify(signin_info));
+
     $( "#login" ).hide();
+    $("#logged_window").show();
   } else {
-    if ( $( "#login .input_username" ).val() !== "admin" ) {
+    if ( $( "#login .input_username" ).val().trim().toLowerCase() !== "admin" ) {
       $( "#login .input_username" ).css( "border-color", "#F00" ).addClass( "blinking_input" );
     }
 
@@ -28,4 +48,10 @@ function check_enter( e ) {
     $( ".btn_login" ).focus();
     login_click();
   }
+}
+
+function logout() {
+  localStorage.clear();
+  $("#logged_window").hide();
+  $( "#login" ).show();
 }
